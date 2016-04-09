c3da8ed36a6f24f4cc6606ddebfec1ac58b9bbd4
Consigliere <joey.corleone92@yahoo.com>
2016-04-09 07:29:56 +0100

    fix: major bugs

--- lib/sequel_enum.rb
+++ lib/sequel_enum.rb
@@ -9,29 +9,43 @@ module Sequel
 
         def enum(column, values)
           self.enums = {}
+
           if values.is_a? Hash
             values.each do |key,val|
               raise ArgumentError, "index should be a symbol, #{key} provided which it's a #{key.class}" unless key.is_a? Symbol
               raise ArgumentError "value should be numeric, #{val} provided which it's a #{val.class}" unless val.is_a? Fixnum
             end
           elsif values.is_a? Array
-            values = Hash[values.map.with_index { |v, i| [i,v] }]
+            values = Hash[values.map.with_index { |v, i| [v, i] }]
           else
-            raise ArgumentError, "#enum expects the second argument to be an array of symbols or a hash like { index => :value }"
+            raise ArgumentError, "#enum expects the second argument to be an array of symbols or a hash like { :literal => number }"
+          end
+
+          define_method :[]= do |key, value|
+            if column.to_sym == key.to_sym
+              index = self.class.enums[column].rassoc(value)
+              @value = (index && index.first)
+            end
+            super(key, value)
           end
 
           define_method "#{column}=" do |value|
-            index = self.class.enums[column].rassoc(value)
-            self[column] = index && index.first
+            index = self.class.enums[column].assoc(value)
+            @value = (index && index.first)
+            self[column] = self.class.enums[column].fetch(@value, nil)
           end
 
           define_method "#{column}" do
-            self.class.enums[column].fetch(self[column], nil)
+            unless @value
+              index = self.class.enums[column].rassoc(self[column])
+              @value = (index && index.first)
+            end
+            @value
           end
 
           values.each do |key, value|
-            define_method "#{value}?" do
-              self.send(column) == value
+            define_method "#{key}?" do
+              self.send(column) == key
             end
           end
           self.enums[column] = values
