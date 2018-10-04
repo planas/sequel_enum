e75ac10e9404801660022cb2f953b926b6938270
Rodrigo Pinto <rodrigo.pinto@blacklane.com>
2018-10-04 17:32:31 +0200

    Adjust spec table creation to make test suite pass.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,19 +2,17 @@ require 'sequel'
 require 'sqlite3'
 require 'sequel_enum'
 
-DB = Sequel.connect('sqlite://test.db')
-
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.order = 'random'
 
-  config.before(:suite) do
-    DB.create_table :items do
-      primary_key :id
-      column :name, String
-      column :condition, Integer
-      column :edition, Integer
-    end
+  DB = Sequel.sqlite
+
+  DB.create_table :items do
+    primary_key :id
+    column :name, String
+    column :condition, Integer
+    column :edition, Integer
   end
 
   config.after(:suite) do
